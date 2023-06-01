7b2e9efb33bc23a5549b70b89b99e48159820d61
Danishaik <danishaikh326474@gmail.com>
2023-06-02 02:31:05 +0500

    adding files

---
+++ CustomAuthenticationMVC/CustomAuthenticationMVC/Controllers/AccountController.cs
@@ -0,0 +1,169 @@
+﻿using CustomAuthenticationMVC.CustomAuthentication;
+using CustomAuthenticationMVC.DataAccess;
+using CustomAuthenticationMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace CustomAuthenticationMVC.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login(string ReturnUrl = "")
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return LogOut();
+            }
+            ViewBag.ReturnUrl = ReturnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginView loginView, string ReturnUrl = "")
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(loginView.UserName, loginView.Password))
+                {
+                    var user = (CustomMembershipUser)Membership.GetUser(loginView.UserName, false);
+                    if (user != null)
+                    {
+                        CustomSerializeModel userModel = new Models.CustomSerializeModel()
+                        {
+                            UserId = user.UserId,
+                            FirstName = user.FirstName,
+                            LastName = user.LastName,
+                            RoleName = user.Roles.Select(r => r.RoleName).ToList()
+                        };
+
+                        string userData = JsonConvert.SerializeObject(userModel);
+                        FormsAuthenticationTicket authTicket = new FormsAuthenticationTicket
+                            (
+                            1, loginView.UserName, DateTime.Now, DateTime.Now.AddMinutes(15), false, userData
+                            );
+
+                        string enTicket = FormsAuthentication.Encrypt(authTicket);
+                        HttpCookie faCookie = new HttpCookie("Cookie1", enTicket);
+                        Response.Cookies.Add(faCookie);
+                    }
+
+                    if (Url.IsLocalUrl(ReturnUrl))
+                    {
+                        return Redirect(ReturnUrl);
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Something Wrong : Username or Password invalid ^_^ ");
+            return View(loginView);
+        }
+
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(RegistrationView registrationView)
+        {
+            bool statusRegistration = false;
+            string messageRegistration = string.Empty;
+
+            if (ModelState.IsValid)
+            {
+                // Email Verification
+                string userName = Membership.GetUserNameByEmail(registrationView.Email);
+                if (!string.IsNullOrEmpty(userName))
+                {
+                    ModelState.AddModelError("Warning Email", "Sorry: Email already Exists");
+                    return View(registrationView);
+                }
+                var user = new User();
+                //Save User Data 
+                using (AuthenticationDB dbContext = new AuthenticationDB())
+                {
+                    user = new User()
+                    {
+                        Username = registrationView.Username,
+                        FirstName = registrationView.FirstName,
+                        LastName = registrationView.LastName,
+                        Email = registrationView.Email,
+                        Password = registrationView.Password,
+                        ActivationCode = Guid.NewGuid(),
+                    };
+
+                    dbContext.Users.Add(user);
+                    dbContext.SaveChanges();
+                }
+
+                //Verification Email
+                //VerificationEmail(registrationView.Email, registrationView.ActivationCode.ToString());
+                messageRegistration = "Your account has been created successfully. ^_^";
+                ActivationAccount(user.ActivationCode.ToString());
+                statusRegistration = true;
+            }
+            else
+            {
+                messageRegistration = "Something Wrong!";
+            }
+            ViewBag.Message = messageRegistration;
+            ViewBag.Status = statusRegistration;
+
+            return View(registrationView);
+        }
+
+        [HttpGet]
+        public ActionResult ActivationAccount(string id)
+        {
+            bool statusAccount = false;
+            using (AuthenticationDB dbContext = new DataAccess.AuthenticationDB())
+            {
+                var userAccount = dbContext.Users.Where(u => u.ActivationCode.ToString().Equals(id)).FirstOrDefault();
+
+                if (userAccount != null)
+                {
+                    userAccount.IsActive = true;
+                    dbContext.SaveChanges();
+                    statusAccount = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Something Wrong !!";
+                }
+
+            }
+            ViewBag.Status = statusAccount;
+            return View();
+        }
+
+        public ActionResult LogOut()
+        {
+            HttpCookie cookie = new HttpCookie("Cookie1", "");
+            cookie.Expires = DateTime.Now.AddYears(-1);
+            Response.Cookies.Add(cookie);
+
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account", null);
+        }
+
+    }
+}
